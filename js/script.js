603642ff46ceb40fe8da81a1265e604dd37e6b8a
sueselle <sue@thehappyuser.com>
2023-11-27 12:05:50 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var button = document.querySelector(".show-cat");
+//console.log(button);
+var cat = document.querySelector(".cat");
+//console.log(cat);
+button.addEventListener("click", function () {
+  cat.classList.add("show");
+});
